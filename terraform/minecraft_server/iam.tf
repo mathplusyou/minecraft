fdae185c01f00e8203ac61d7559fbfe248fe8619
Matthew Ramirez <matthewxprogramming@gmail.com>
2021-05-19 00:19:36 -0700

    mod

--- terraform/minecraft_server/iam.tf
+++ terraform/minecraft_server/iam.tf
@@ -5,7 +5,7 @@
 data aws_iam_policy_document ec2_assume_role {
   statement {
     effect = "Allow"
-    actions = ["stst:AssumeRole"]
+    actions = ["sts:AssumeRole"]
     principals {
       type = "Service"
       identifiers = ["ec2.amazonaws.com"]
