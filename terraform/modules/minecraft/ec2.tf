fc7ecc53b441db03876cd29e05af9400486b0445
Matthew Ramirez <matthewramirez@Matthews-MacBook-Pro.local>
2021-12-30 17:36:18 -0800

    mod

--- terraform/modules/minecraft/ec2.tf
+++ terraform/modules/minecraft/ec2.tf
@@ -34,12 +34,14 @@ resource "aws_autoscaling_group" "minecraft" {
 }
 
 resource "aws_launch_template" "minecraft" {
-  name_prefix          = "minecraft"
-  image_id             = var.ec2_ami
-  instance_type        = var.ec2_instance_type
-  iam_instance_profile = aws_iam_instance_profile.minecraft.name
-  key_name             = aws_key_pair.minecraft.key_name
-  security_groups      = [aws_security_group.minecraft.id]
+  name_prefix   = "minecraft"
+  image_id      = var.ec2_ami
+  instance_type = var.ec2_instance_type
+  iam_instance_profile {
+    name = aws_iam_instance_profile.minecraft.name
+  }
+  key_name        = aws_key_pair.minecraft.key_name
+  security_groups = [aws_security_group.minecraft.id]
 
   lifecycle {
     create_before_destroy = true
